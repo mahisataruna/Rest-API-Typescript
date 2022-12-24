a3cf93ebc2226835d7ecc87405e41b5a697d9906
mahisataruna <mahisataruna4@gmail.com>
2022-12-24 08:18:07 +0700

    Setup ESLint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const app: Application = express();
-const port : Number = 4000;
+const app: Application = express()
+const port: Number = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: 200 });
-});
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: 200, data: 'Hello World' })
+})
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
-
-
-// End video 1 : Installing postman menit ke 20:41
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
