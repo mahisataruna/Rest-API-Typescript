70808f9566909dd9c8ff44f8655648a65b9d0d86
mahisataruna <mahisataruna4@gmail.com>
2022-12-29 23:59:22 +0700

    Add userModel

---
+++ src/models/user.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    user_id: { type: String, unique: true },
+    email: { type: String, unique: true },
+    name: { type: String, default: '' },
+    password: { type: String, default: '' },
+    role: { type: String, default: 'regular' }
+  },
+  { timestamps: true }
+)
+
+const userModel = mongoose.model('user', userSchema)
+
+export default userModel
